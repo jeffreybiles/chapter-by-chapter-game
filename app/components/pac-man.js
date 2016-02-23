c431b9c2e43538e27c95795eb33076e5e18ad2a9
jeffreybiles <bilesjeffrey@gmail.com>
2016-02-23 02:12:46 -0600

    chapter 5- borders

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -40,10 +40,28 @@ export default Ember.Component.extend(KeyboardShortcuts, {
     ctx.clearRect(0, 0, screenPixelWidth, screenPixelHeight)
   },
 
-  movePacMan: function(direction, amount){
+  movePacMan(direction, amount){
     this.incrementProperty(direction, amount);
+
+    if(this.collidedWithBorder()) {
+      this.decrementProperty(direction, amount)
+    }
+
     this.clearScreen();
     this.drawCircle();
+  },
+
+  collidedWithBorder: function(){
+    let x = this.get('x');
+    let y = this.get('y');
+    let screenHeight = this.get('screenHeight');
+    let screenWidth = this.get('screenWidth');
+
+    let pacOutOfBounds = x < 0 ||
+                         y < 0 ||
+                         x >= screenWidth ||
+                         y >= screenHeight
+    return pacOutOfBounds
   },
 
   keyboardShortcuts: {
